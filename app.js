53338be62fdd21f4b915ddebd1150c129d0e40ae
SamanthaSortomme <sor23001@byui.edu>
2023-05-03 12:53:46 -0700

    working again, but not oauth

--- app.js
+++ app.js
@@ -62,21 +62,21 @@ app.get('/actors', requiresAuth(), (req, res) => {
   })
 })
 //==================================================================
-// app
-// .use(bodyParser.json())
-// .use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   next();
-// })
-// .use('/', require('./routes'));
-// process.on('uncaughtException', (err, origin) => {
-//   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-// });
+app
+.use(bodyParser.json())
+.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
+.use('/', require('./routes'));
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
 
 
 mongodb.initDb((err, mongodb) => {
